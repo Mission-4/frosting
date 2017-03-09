7b89e9c5476185e3eb3e7a964f95e09ab24ae8aa
Sam Podlogar <sam@podlogar.com>
2017-03-08 21:46:58 -0700

    Added the Base Service Provider

---
+++ src/FrostingServiceProvider.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Mission4\Frosting;
+
+use Illuminate\Support\ServiceProvider;
+
+class FrostingServiceProvider implements ServiceProvider
+{
+    public function boot()
+    {
+        // Register Asserts
+    }
+}
